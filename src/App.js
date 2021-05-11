7d31a6ed2a1e8a78908d8d41ff3d39581eeb705c
Jwill617Boston <77422031+Jwill617Boston@users.noreply.github.com>
2021-05-10 20:01:47 -0400

    Restart

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to This shit </h1>
-        </header>
-        <p className="App-intro">
-          I just wanted to play around with the words{" "}
-          <code>Link to No Where</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="App">
+            <header className="App-header">
+               <img src={logo} className="App-logo" alt="logo" />
+               <h1 className="App-title">Welcome to This shit </h1>
+            </header>
+            <p className="App-intro">
+               I just wanted to play around with the words{" "}
+               <code>Link to No Where</code> and save to reload.
+            </p>
+         </div>
+      );
+   }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom";
 import "./index";
 import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-
 import App from "./App";
-import Movies from "./components/Movies";
 
 ReactDOM.render(
-  <>
-    <Movies />
-    <App />
-  </>,
-  document.getElementById("root")
+   <>
+      <App />
+   </>,
+   document.getElementById("root")
 );
 
 registerServiceWorker();
